be9c5dff789375bc5996dd8e720ac76bb773ded8
Goppika <goppikaloganadane@gmail.com>
2026-07-02 22:50:31 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+/*To get a pattern like this
+11111
+2222
+333
+44
+5*/
+public class Pattern2 {
+    public static void main(String args[]){
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
